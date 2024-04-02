5b7c3b9ee19a4533b7b52057e7393497fb4b3582
Robert Bruno <robert.alexander.bruno@gmail.com>
2024-04-01 21:28:39 -0400

    fix: update nginx scripts

--- scripts/certbot-cli.sh
+++ scripts/certbot-cli.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# Si la variable DEBUG existe, se mostraran las órdenes y sus argumentos mientras se ejecutan.
+# If the DEBUG variable exists, the commands and their arguments will be displayed while they are executed.
 [ -n "${DEBUG:-}" ] && set -x
 
 export params=${params:-"certificates"}
